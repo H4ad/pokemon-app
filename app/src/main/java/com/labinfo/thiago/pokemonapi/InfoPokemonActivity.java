6a4cf420282182a82a8601af979eec33da73f288
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-08-23 16:51:33 -0300

    Adicionando BindView aos elementos das Activitys

--- app/src/main/java/com/labinfo/thiago/pokemonapi/InfoPokemonActivity.java
+++ app/src/main/java/com/labinfo/thiago/pokemonapi/InfoPokemonActivity.java
@@ -2,12 +2,21 @@ package com.labinfo.thiago.pokemonapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class InfoPokemonActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_pokeName)
+    TextView textPokeName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_pokemon);
+
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/labinfo/thiago/pokemonapi/MainActivity.java
+++ app/src/main/java/com/labinfo/thiago/pokemonapi/MainActivity.java
@@ -2,12 +2,25 @@ package com.labinfo.thiago.pokemonapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_find)
+    Button buttonFind;
+
+    @BindView(R.id.edit_pokeNumber)
+    EditText editPokeNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
