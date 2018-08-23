8825f0f24d18dba24ae8699a53cbbed16367af18
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-08-23 16:59:01 -0300

    Adicionado método para o botão de procurar Pokemon

--- app/src/main/java/com/labinfo/thiago/pokemonapi/MainActivity.java
+++ app/src/main/java/com/labinfo/thiago/pokemonapi/MainActivity.java
@@ -4,9 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_find)
+    public void findPokemon() {
+        int pokeNumber = Integer.parseInt(editPokeNumber.getText().toString());
+
+        if (pokeNumber <= 0 || pokeNumber > 255) {
+            Toast.makeText(this, "Número do pokemon inválido!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO: Implementar chamada à API do Pokemon, pegar o nome do Pokemon e exibir na InfoPokemonActivity
     }
 }
